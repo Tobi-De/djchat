56e5fa3745eb69de12b20375ab65a32187fec2a9
Tobi DEGNON <tobidegnon@protonmail.com>
2020-08-13 12:20:47 +0100

    add router manually

---
+++ frontend/src/components/UserAuth.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "UserAuth"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Chat from './components/Chat'
+import UserAuth from "@/components/UserAuth";
 
 Vue.config.productionTip = false
 
+
+const router = new VueRouter({
+  routes: [
+    { path: '/chats', name: 'Chat',component: Chat },
+    { path: '/auth', name: 'UserAuth' ,component: UserAuth },
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (sessionStorage.getItem('authToken') !== null || to.path === '/auth') {
+    next()
+  } else {
+    next('/auth')
+  }
+})
+
+Vue.use(VueRouter)
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
